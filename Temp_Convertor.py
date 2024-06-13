97b4629de496dbac807665f7a0ace12fc21349e3
TaariqHoo24 <st24580@howick.school.nz>
2024-06-13 16:25:30 +1200

    check_temp function added
    Added a check_temp function for converting from Fahrenheit to Celsius, to see if the value added by the user is above absolute zero. If the value is lower than absolute zero, then it will return an error message. Made the history/export button appear as disabled. Made the error message blank.

--- Temp_Convertor.py
+++ Temp_Convertor.py
@@ -19,8 +19,8 @@ class temp_conv:
 
         erorr = 'Please enter number'
 
-        self.temp_erorr = tk.Label(self.temp_frame, text = erorr, fg = 'red')
-        self.temp_erorr.grid(row = 3)
+        self.temp_error = tk.Label(self.temp_frame, text = '', fg = 'red')
+        self.temp_error.grid(row = 3)
 
         self.button_frame = tk.Frame(self.temp_frame)
         self.button_frame.grid(row = 4)
@@ -28,14 +28,33 @@ class temp_conv:
         self.temp_cel = tk.Button(self.button_frame, text = 'To Degrees C', bg = "#990099", fg = "#FFFFFF", font = ('Arial', 14, 'bold'), width = 14, command = self.to_degtC)
         self.temp_cel.grid(row = 0, column = 0, padx = 5, pady = 5)
 
-        self.temp_far = tk.Button(self.button_frame, text = 'To Farenheit', bg = "#009900", fg = "#FFFFFF", font = ('Arial', 14, 'bold'), width = 14, command = self.to_degF)
+        self.temp_far = tk.Button(self.button_frame, text = 'To Farenheit', bg = "#009900", fg = "#FFFFFF", font = ('Arial', 14, 'bold'), width = 14)
         self.temp_far.grid(row = 0, column = 1, padx = 5, pady = 5)
 
         self.temp_help = tk.Button(self.button_frame, text = 'Help / Info', bg = "#CC6600", fg = "#FFFFFF", font = ('Arial', 14, 'bold'), width = 14)
         self.temp_help.grid(row = 1, column = 0, padx = 5, pady = 5)
 
-        self.temp_hist = tk.Button(self.button_frame, text = 'History / Export', bg = "#004C99", fg = "#FFFFFF", font = ('Arial', 14, 'bold'), width = 14)
+        self.temp_hist = tk.Button(self.button_frame, text = 'History / Export', bg = "#004C99", fg = "#FFFFFF", font = ('Arial', 14, 'bold'), width = 14, state = 'disabled')
         self.temp_hist.grid(row = 1, column = 1, padx = 5, pady = 5)
+
+    def check_temp(self, min_value):
+        error = 'Please enter a number that is more than {}'.format(min_value)
+
+        try:
+            response = self.temp_entry.get()
+            response = float(response)
+
+            if response < min_value:
+                self.temp_error.config(text = error)
+            else:
+                return response
+        
+        except ValueError:
+            self.temp_error.config(text = error)
+
+    def to_degtC(self):
+
+        self.check_temp(-459)
     
 root = tk.Tk()
 root.title('Temperature Convertor')
